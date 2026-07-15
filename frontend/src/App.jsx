8096c037820fbc130b07878712ccf708c98e6f53
kho903 <gmldnr2222@naver.com>
2026-07-15 22:15:25 +0900

    chore: initialize React project #1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>JihunPage</h1>
+      <p>React로 만드는 자기소개 페이지입니다.</p>
+
+      <button type="button" className="btn btn-primary">
+        Bootstrap 확인
+      </button>
+    </div>
+  );
+}
+
+export default App;

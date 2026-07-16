8d7585aed063698aef0092561289fa785fe2f316
kho903 <gmldnr2222@naver.com>
2026-07-16 15:18:20 +0900

    feat: set up page routing #3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from "react-router";
+
+import Gallery from "./pages/Gallery";
+import Guestbook from "./pages/Guestbook";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
-    <div className="container mt-5">
-      <h1>JihunPage</h1>
-      <p>React로 만드는 자기소개 페이지입니다.</p>
-
-      <button type="button" className="btn btn-primary">
-        Bootstrap 확인
-      </button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/guestbook" element={<Guestbook />}></Route>
+      <Route path="/gallery" element={<Gallery />}></Route>
+    </Routes>
   );
 }
 

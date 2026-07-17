ab6937502f2713f89fb975412b66a9014e3c813a
kho903 <gmldnr2222@naver.com>
2026-07-17 22:44:14 +0900

    feat: add About section to Home page #4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../components/home/About";
 import Hero from "../components/home/Hero";
 
 function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }

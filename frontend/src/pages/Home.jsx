967c3ef02b6ee45b2fa18112967c15bb6ec1dd68
kho903 <gmldnr2222@naver.com>
2026-07-18 12:36:50 +0900

    feat: add Projects section to Home page #4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import About from "../components/home/About";
 import Experience from "../components/home/Experience";
 import Hero from "../components/home/Hero";
 import Skills from "../components/home/Skills";
+import Project from "../components/home/Project";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <About />
       <Experience />
       <Skills />
+      <Project />
     </>
   );
 }

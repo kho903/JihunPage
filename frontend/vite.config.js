ce97af84dcd0a702468bd40acc075b5ee1ae586b
kho903 <gmldnr2222@naver.com>
2026-07-19 18:28:33 +0900

    chore: configure gallery proxy #21

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
         target: "http://localhost:8080",
         changeOrigin: true,
       },
+
+      "/uploads": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
     },
   },
 });

14f0aeeb50be48408529c7861c9d055800e92216
kho903 <gmldnr2222@naver.com>
2026-07-19 15:41:20 +0900

    feat: validate gallery image uploads #19

---
+++ backend/src/main/java/com/sisa/jihunpage/gallery/exception/InvalidGalleryImageException.java
@@ -0,0 +1,7 @@
+package com.sisa.jihunpage.gallery.exception;
+
+public class InvalidGalleryImageException extends RuntimeException {
+	public InvalidGalleryImageException(String message) {
+		super(message);
+	}
+}
